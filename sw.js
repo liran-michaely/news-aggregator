e1c340d5528e26588563053c11c8e5932825b4f9
liran-michaely <34343153+liran-michaely@users.noreply.github.com>
2025-08-09 13:58:02 +0300

    Added new files

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = "news-aggregator-v2";
+const PRECACHE = ["./", "./index.html", "./manifest.webmanifest", "./icons/icon-192.png", "./icons/icon-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(PRECACHE)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+});
+
+self.addEventListener("fetch", (e) => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    // cache-first for own assets
+    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  }
+});
