1d554964262b5b1419160ea0dd954502bb63d1a5
liran-michaely <34343153+liran-michaely@users.noreply.github.com>
2025-08-09 22:54:18 +0300

    v11 with new sources

---
+++ sw.js
@@ -0,0 +1,17 @@
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('v1').then(c => c.addAll(['./','./index.html','./manifest.webmanifest'])));
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== 'v1' && caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  // Only cache same-origin GETs
+  if (e.request.method === 'GET' && url.origin === location.origin) {
+    e.respondWith(
+      caches.match(e.request).then(res => res || fetch(e.request))
+    );
+  }
+});
